bcc7b61edfbb0a7eff545bf8e82926cf69f3fc26
Artur Mucha <arti.mucha@gmail.com>
2021-10-15 07:33:20 +0200

    add categories

---
+++ constans/filters.js
@@ -0,0 +1,33 @@
+
+export const categories = [
+  {
+    name: 'Biżuteria',
+    value: 'jewelry',
+    slug: 'bizuteria'
+  },
+  {
+    name: 'Dokumenty',
+    value: 'ids',
+    slug: 'dokumenty'
+  },
+  {
+    name: 'Elektronika',
+    value: 'electronics',
+    slug: 'elektronika'
+  },
+  {
+    name: 'Ubrania',
+    value: 'clothes',
+    slug: 'ubrania'
+  },
+  {
+    name: 'Zwierzęta',
+    value: 'animals',
+    slug: 'zwierzeta'
+  },
+  {
+    name: 'Pozostałe',
+    value: 'others',
+    slug: 'pozostale'
+  },
+];
