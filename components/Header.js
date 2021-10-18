91fc999fae7b985f7f356cf33e38554642bf6341
Artur Mucha <arti.mucha@gmail.com>
2021-10-18 08:34:06 +0200

    add user and apps icons

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,22 @@
 import Link from 'next/link';
-import { header, container } from 'styles/Layout.module.css'
+import { header, menu, containerFlex } from 'styles/Layout.module.css'
+
+import AppsIcon from '../public/icons/apps.svg'
+import UserIcon from '../public/icons/user.svg'
 
 const Header = ({children}) => {
   return (
     <header className={header}>
-      <nav className={container}>
+      <nav className={containerFlex}>
         <Link href="/"><a>Zgubione.com</a></Link>
+        <ul className={menu}>
+          <li>
+            <AppsIcon />
+          </li>
+          <li>
+            <UserIcon />
+          </li>
+        </ul>
       </nav>
       { children }
     </header>
