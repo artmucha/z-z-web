f17e83724082a9184049c836634023026a00bf1d
Artur Mucha <arti.mucha@gmail.com>
2021-11-17 17:33:52 +0100

    add messege icon to header

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,19 @@
 import Link from 'next/link';
 
-import { header, pageHeader, containerFlex } from 'styles/Layout.module.css';
+import { header, pageHeader, headerIcons, containerFlex } from 'styles/Layout.module.css';
 
 import UserIcon from '../public/icons/user.svg';
+import MessegeIcon from '../public/icons/messege.svg';
 
 const Header = ({ children, homePage }) => {
   return (
     <header className={homePage ? header : pageHeader}>
       <nav className={containerFlex}>
         <Link href="/"><a>Zgubione.com</a></Link>
-        <Link href="/logowanie"><a title="Zaloguj się"><UserIcon /></a></Link>
+        <div className={headerIcons}>
+          <Link href="/messeges"><a title="Wiadomości"><MessegeIcon /></a></Link>
+          <Link href="/logowanie"><a title="Zaloguj się"><UserIcon /></a></Link>
+        </div>
       </nav>
       { children }
     </header>
