d6a2da358a88f1f054cda6523686867590399b7f
Artur Mucha <arti.mucha@gmail.com>
2021-11-17 16:08:00 +0100

    update card component

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,17 @@
 import Image from 'next/image';
+import { imageLoader } from 'utils/helpers'
 
 import { card, postCard, cardTitle, cardMeta } from 'styles/Card.module.css';
 
 const Card = ({type, title, image, width, height, category, date}) => {
+
   return (
     <div className={ type === 'icon' ? card : postCard }>
-      {/* <Image src={image} width={width} height={height} /> */}
+      { type !== 'icon' ? 
+        <Image loader={imageLoader} src={image} width={width} height={height} /> 
+        :
+        <Image src={image} width={width} height={height} /> 
+      }
       <h3 className={cardTitle}>{title}</h3>
       { type !== 'icon' ? (
         <div>
